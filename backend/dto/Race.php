97dda525cd0c5e3bf53aa31eae02a3548913876f
Adrian Goral <adrian.goral@gmail.com>
2024-07-22 20:20:55 +0200

    refactor: Remove hard-coded defaultPerks array from Race constructor

--- backend/dto/Race.php
+++ backend/dto/Race.php
@@ -17,24 +17,6 @@ abstract class Race {
 
     public function __construct(RaceName $name) {
         $this->name = $name;
-        $this->defaultPerks = array(
-            new Perk(PerkType::Main, PerkName::WalkaWrecz, 0),
-            new Perk(PerkType::Main, PerkName::UmiejetnosciStrzeleckie, 0),
-            new Perk(PerkType::Main, PerkName::Krzepa, 0),
-            new Perk(PerkType::Main, PerkName::Odpornosc, 0),
-            new Perk(PerkType::Main, PerkName::Zrecznosc, 0),
-            new Perk(PerkType::Main, PerkName::Inteligencja, 0),
-            new Perk(PerkType::Main, PerkName::SilaWoli, 0),
-            new Perk(PerkType::Main, PerkName::Oglada, 0),
-            new Perk(PerkType::Secondary, PerkName::Akcja, 0),
-            new Perk(PerkType::Secondary, PerkName::Zywotnosc, 0),
-            new Perk(PerkType::Secondary, PerkName::Sila, 0),
-            new Perk(PerkType::Secondary, PerkName::Wytrzymalosc, 0),
-            new Perk(PerkType::Secondary, PerkName::Szybkosc, 0),
-            new Perk(PerkType::Secondary, PerkName::PunktyObledu, 0),
-            new Perk(PerkType::Secondary, PerkName::PunktyPrzeznaczenia, 0),
-            new Perk(PerkType::Secondary, PerkName::PunktySzczescia, 0)
-        );
         $this->setupDefaultPerks();
     }
 
