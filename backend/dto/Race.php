1abca5ddb5ebabb92a3d8da63b233784ccd8ad3c
Adrian Goral <adrian.goral@gmail.com>
2024-07-22 20:23:35 +0200

    refactor: Update Race class to use protected visibility for properties

--- backend/dto/Race.php
+++ backend/dto/Race.php
@@ -12,11 +12,29 @@ enum RaceName {
 }
 
 abstract class Race {
-    private RaceName $name;
-    private array $defaultPerks;
+    protected RaceName $name;
+    protected array $defaultPerks;
 
     public function __construct(RaceName $name) {
         $this->name = $name;
+        $this->defaultPerks = array(
+            new Perk(PerkType::Main, PerkName::WalkaWrecz, 0),
+            new Perk(PerkType::Main, PerkName::UmiejetnosciStrzeleckie, 0),
+            new Perk(PerkType::Main, PerkName::Krzepa, 0),
+            new Perk(PerkType::Main, PerkName::Odpornosc, 0),
+            new Perk(PerkType::Main, PerkName::Zrecznosc, 0),
+            new Perk(PerkType::Main, PerkName::Inteligencja, 0),
+            new Perk(PerkType::Main, PerkName::SilaWoli, 0),
+            new Perk(PerkType::Main, PerkName::Oglada, 0),
+            new Perk(PerkType::Secondary, PerkName::Akcja, 0),
+            new Perk(PerkType::Secondary, PerkName::Zywotnosc, 0),
+            new Perk(PerkType::Secondary, PerkName::Sila, 0),
+            new Perk(PerkType::Secondary, PerkName::Wytrzymalosc, 0),
+            new Perk(PerkType::Secondary, PerkName::Szybkosc, 0),
+            new Perk(PerkType::Secondary, PerkName::PunktyObledu, 0),
+            new Perk(PerkType::Secondary, PerkName::PunktyPrzeznaczenia, 0),
+            new Perk(PerkType::Secondary, PerkName::PunktySzczescia, 0)
+        );
         $this->setupDefaultPerks();
     }
 
