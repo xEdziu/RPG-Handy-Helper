87dd1d5a6c786a506dcbf03a7797a57cfd56614d
Adrian Goral <adrian.goral@gmail.com>
2024-07-25 07:32:25 +0200

    Corrected Ping class to correctly check status of database connection

--- backend/Database/Ping.php
+++ backend/Database/Ping.php
@@ -6,10 +6,22 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 
 use Eddy\RpgHandyHelper\Database\DatabaseConnector;
 
-$database = new DatabaseConnector();
+class Ping {
+    private $databaseConnector;
 
-if ($database === null) {
-    echo "Database connection failed";
-} else {
-    echo "Database connection successful";
+    public function __construct() {
+        $this->databaseConnector = new DatabaseConnector();
+
+        if ($this->databaseConnector === null || $this->databaseConnector->db === null) {
+            echo "\nDatabase connection failed: DatabaseConnector or its db property is null.\n";
+        } elseif ($this->databaseConnector->db->connect_error) {
+            // If not null, then check for a connection error
+            echo "Database connection failed: " . $this->databaseConnector->db->connect_error;
+        } else {
+            echo "Database connection successful";
+        }
+    }
 }
+
+// Usage
+new Ping();
