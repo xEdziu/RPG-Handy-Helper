7e5d44ec0282ffb1020894ba4d4ca2d22f322288
Adrian Goral <adrian.goral@gmail.com>
2024-07-27 00:00:19 +0200

    Refactor error reporting depending on the environment

--- backend/Database/DatabaseConnector.php
+++ backend/Database/DatabaseConnector.php
@@ -17,9 +17,13 @@ try {
     die($error);
 }
 
-//TODO: Remove this in production
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+if ($_ENV['PRODUCTION'] === "true") {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+} else {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
 class DatabaseConnector {
     private string $host;
