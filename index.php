352375d94b2ea466ddb827fd49c9f4c020317f79
MANDRW <mandrzejewski132@gmail.com>
2024-07-28 18:49:55 +0200

    start with learn more page

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
         <h3>A tool for your campaigns</h3>
     </header>
     <main class="andika-regural">
-        <button class="linkBtn" onclick="location.href='html/login.html'">Login</button>
-        <button class="linkBtn" onclick="location.href='html/register.html'">Register</button>
+        <button class="linkBtn" onclick="location.href='frontend/html/login.html'">Login</button>
+        <button class="linkBtn" onclick="location.href='frontend/html/register.html'">Register</button>
         <br>
-        <button class="linkBtn" onclick="location.href='about.html'">Learn more</button>
+        <button class="linkBtn" onclick="location.href='frontend/html/about.html'">Learn more</button>
         <button class="linkBtn" onclick="location.href='demo.html'">Demo campaign</button>
     </main>
     <footer class="andika-regular">
