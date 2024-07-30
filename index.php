428d0c01fca26659883661ac1507666afd8f2cda
MANDRW <mandrzejewski132@gmail.com>
2024-07-31 00:44:29 +0200

    final version of landing page without rwd

--- index.php
+++ index.php
@@ -7,34 +7,53 @@
     <title>RPG Handy Helper</title>
     <link rel="icon" type="image/x-icon" href="/frontend/img/dark-bg-ico.ico">
 </head>
+
 <body>
-    <header class="andika-bold">
-        <div class="logo">
-        <h1><a href="/index.php" class="link">RPG Handy<br>Helper</a></h1> 
-        <h3>A tool for your campaigns</h3>
+    <div class="content-wrapper">
+        <div class=container>
+            <div class="column1">
+                <header class="andika-bold">
+                    <div class="title">
+                    <h1><a href="/index.php" class="link">RPG Handy<br>Helper</a></h1> 
+                    <h3>A tool for your campaigns</h3>
+                    </div>
+                </header>
+
+                <main class="andika-regural">
+                    <div class="buttons1">
+                        <button class="linkBtn" onclick="location.href='frontend/html/login.html'">Login</button>
+                        <button class="linkBtn" onclick="location.href='frontend/html/register.html'">Register</button>
+                    </div>
+                    <div class="buttons2">
+                        <button class="linkBtn" onclick="location.href='demo.html'">Join</button>   
+                        <button class="linkBtn" onclick="location.href='frontend/html/about.html'">Learn more</button>        
+                    </div>
+                    <div class="buttons3">
+                        <button class="linkBtn" onclick="location.href='demo.html'">Demo campaign</button>
+                        <button class="linkBtn" onclick="location.href='demo.html'">Authors</button>
+                    </div>
+                    
+                </main>
+            </div>
+
+            <div class="column2">
+                <img src="/frontend/img/dark-bg.svg" alt="swords">
+            </div>
         </div>
-        <img src="/frontend/img/dark-bg-ico.svg" alt="swords">
-    </header>
-   
-    
-    <main class="andika-regural">
-        <button class="linkBtn" onclick="location.href='frontend/html/login.html'">Login</button>
-        <button class="linkBtn" onclick="location.href='frontend/html/register.html'">Register</button>
-        <br>
-        <button class="linkBtn" onclick="location.href='frontend/html/about.html'">Learn more</button>
-        <button class="linkBtn" onclick="location.href='demo.html'">Demo campaign</button>
-    </main>
-    <footer class="andika-regular">
-        <h2>Authors:</h2>
-        <p>Wiktor Siepka, Mateusz Andrzejewski, Szymon Nowicki</p>
-        <p><a class="personalLink" href="https://github.com/xEdziu">Adrian Goral</a>, Mateusz Zubrzycki</p>
-    </footer>
-    <div class="popup" id="cookies">
-    <span class="pinfo andika-regular" >RPG Handy Helper uses cookies in order to work properly [&nbsp 
-        <a href="https://allaboutcookies.org/what-is-a-cookie" class="pinfo andika-regular">More Informations</a>&nbsp]
-    </span>
-    <span class="pbtn andika-regular" id="cdis">I Agree</span>
-</div>
+
+        <!-- <footer class="andika-regular">
+            <h2>Authors:</h2>
+            <p>Wiktor Siepka, Mateusz Andrzejewski, Szymon Nowicki, <a class="personalLink" href="https://github.com/xEdziu">Adrian Goral</a>, Mateusz Zubrzycki</p>
+        </footer> -->
+
+        <div class="popup" id="cookies">
+            <span class="pinfo andika-regular" >RPG Handy Helper uses cookies in order to work properly [&nbsp 
+                <a href="https://allaboutcookies.org/what-is-a-cookie" class="pinfo andika-regular">More Informations</a>&nbsp]
+            </span>
+            <span class="pbtn andika-regular" id="cdis">I Agree</span>
+        </div>
+    </div>
+
 </body>
 <script>
     document.addEventListener('DOMContentLoaded', (event)=> {
