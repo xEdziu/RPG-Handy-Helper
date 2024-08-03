96f3a4910721dc0c3635d5d9683434a7ad6c5d87
MANDRW <mandrzejewski132@gmail.com>
2024-08-03 23:44:06 +0200

    final version of popup

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event)=> {
     let cookies=localStorage.getItem("cookies");
     let popup= document.getElementById("cookies");
+    let container=document.querySelector(".container");
     if(cookies != 1){
+        container.style.marginTop="9.75vh";
         popup.style.display="flex";
         document.getElementById("cdis").onclick= function(){
             popup.style.display="none";
+            container.style.marginTop="0vh";
             localStorage.setItem("cookies",1)
         };
     }   
--- index.php
+++ index.php
@@ -44,19 +44,19 @@
         </div>
 
        
-        
+       
+
         
     </div>
-  
-   
     <div class="popup" id="cookies">
         <span class="pinfo andika-regular" >RPG Handy Helper uses cookies in order to work properly [&nbsp 
             <a href="https://allaboutcookies.org/what-is-a-cookie" class="pinfo andika-regular">More Informations</a>&nbsp]
         </span>
         <span class="pbtn andika-regular" id="cdis">I Agree</span>
     </div>
-
-    <footer class="andika-regular">
+   
+   
+    <footer class="andika-regular" id="footer">
             <!-- <h2>Authors:</h2> -->
             <!-- <p>Wiktor Siepka, Mateusz Andrzejewski, Szymon Nowicki, <a class="personalLink" href="https://github.com/xEdziu">Adrian Goral</a>, Mateusz Zubrzycki</p> -->
             <p>© <?php echo date("Y"); ?> RPG Handy Helper. All rights reserved.</p>
