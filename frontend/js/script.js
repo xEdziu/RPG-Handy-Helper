1cdbe6fca4fbd42ef7cc381267b92ca78290a263
Sybirsoldat <122818081+Sybirsoldat@users.noreply.github.com>
2024-08-05 12:03:15 +0200

    ok, simple animation works
    IT WORKS (idk why, more testing will be needed)

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -10,20 +10,7 @@ document.addEventListener('DOMContentLoaded', (event)=> {
     }   
 });
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
 
-});
-
-const hiddenElemnets = document.querySelectorAll('.hidden');
-hiddenElemnets.forEach((el)=>observer.observe(el));
 
 
 
