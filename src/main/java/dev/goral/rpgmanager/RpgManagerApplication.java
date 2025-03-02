966a53511d441178ee72e8d336274fee7ab84f55
Adrian Goral <adrian.goral@gmail.com>
2025-03-02 16:50:57 +0100

    Init - files

---
+++ src/main/java/dev/goral/rpgmanager/RpgManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.goral.rpgmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpgManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/goral/rpgmanager/RpgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.goral.rpgmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
