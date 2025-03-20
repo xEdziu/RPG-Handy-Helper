51a51b24e913467bce79242c63490514ca85cbbb
ZuberRS03 <matiz_z@o2.pl>
2025-03-20 23:30:44 +0100

    Poprawki walidacji w dla endpointu createRpgSystems
    Pełny link endointu: http://localhost:8888/api/v1/authorized/admin/rpgSystems/create

--- src/main/java/dev/goral/rpgmanager/rpgSystems/RpgSystems.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/RpgSystems.java
@@ -23,6 +23,8 @@ public class RpgSystems {
     )
     private Long id;
     private String name;
+
+    @Column(length = 1000)
     private String description;
 
     public RpgSystems(String name, String description) {
