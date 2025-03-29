18860f42ef3b7e55b983c990a423e8d8cfd2cf14
Adrian Goral <adrian.goral@gmail.com>
2025-03-29 17:51:05 +0100

    Refactor isUserInGame method parameter annotations for clarity

--- src/main/java/dev/goral/rpgmanager/game/gameUsers/GameUsersRepository.java
+++ src/main/java/dev/goral/rpgmanager/game/gameUsers/GameUsersRepository.java
@@ -1,7 +1,6 @@
 package dev.goral.rpgmanager.game.gameUsers;
 
 import dev.goral.rpgmanager.game.Game;
-import dev.goral.rpgmanager.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -29,5 +28,5 @@ public interface GameUsersRepository extends JpaRepository<GameUsers, Long> {
     long countByGameIdAndRole(@Param("gameId") Long gameId, @Param("role") GameUsersRole role);
 
     @Query("SELECT gu FROM GameUsers gu WHERE gu.game.id = :gameId AND gu.user.id = :userId")
-    Optional<GameUsers> isUserInGame(Long gameId, Long userId);
+    Optional<GameUsers> isUserInGame(@Param("gameId") Long gameId, @Param("userId") Long userId);
 }
