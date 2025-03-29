a525c725b882202a7a70175b7a0972fb436fe174
MANDRW <mandrzejewski132@gmail.com>
2025-03-29 13:05:52 +0100

    weapon DTO's

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/characterWeapon/CpRedCharacterWeaponDTO.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/characterWeapon/CpRedCharacterWeaponDTO.java
@@ -1,4 +1,17 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.characters.characterWeapon;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class CpRedCharacterWeaponDTO {
+    private int weaponId;
+    private int characterId;
+    private String quality;
+    private int ammunition;
+    private String status;
+    private String description;
 }
--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/weapon/CpRedWeaponDTO.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/weapon/CpRedWeaponDTO.java
@@ -1,4 +1,22 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.characters.weapon;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
 public class CpRedWeaponDTO {
+    private String requireSkillId;
+    private String type;
+    private int damage;
+    private int magazineCapacity;
+    private String ammunition;
+    private short la;
+    private short hands;
+    private boolean isHidden;
+    private int price;
+    private String availability;
+    private String description;
 }
