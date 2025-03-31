627f5c1a9e9668b33f787191454e49b76d2c1540
MANDRW <mandrzejewski132@gmail.com>
2025-03-31 19:48:19 +0200

    customArmors

---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/customArmors/CpRedCustomArmors.java
@@ -0,0 +1,44 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.characters.customArmors;
+
+import dev.goral.rpgmanager.game.Game;
+import dev.goral.rpgmanager.rpgSystems.cpRed.characters.armors.CpRedArmorsType;
+import dev.goral.rpgmanager.rpgSystems.cpRed.characters.item.CpRedItemAvailability;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CpRedCustomArmors {
+    @Id
+    @SequenceGenerator(
+            name = "customArmors_sequence",
+            sequenceName = "customArmors_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customArmors_sequence"
+    )
+    private long id;
+    @ManyToOne
+    @JoinColumn(
+            name = "game_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private Game gameId;
+    @Enumerated(EnumType.STRING)
+    private CpRedArmorsType type;
+    private int ob;
+    private int penalty;
+    private int price;
+    @Enumerated(EnumType.STRING)
+    private CpRedItemAvailability availability;
+    private String description;
+}
