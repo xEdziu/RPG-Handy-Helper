9d90c7066dc9d691d0bb2a3e770287c689418515
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-04-07 19:51:26 +0200

    update for pr check

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/characterCustomEquipment/CpRedCharacterCustomEquipmentRepository.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/characterCustomEquipment/CpRedCharacterCustomEquipmentRepository.java
@@ -5,5 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CpRedCharacterCustomEquipmentRepository extends JpaRepository<CpRedCharacterCustomEquipment, Long> {
-    CpRedCharacterCustomEquipment findByName(String name);
 }
