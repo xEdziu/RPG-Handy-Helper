79536b2def118d7a9d455ee4c1fddb839e5d5ae4
MANDRW <mandrzejewski132@gmail.com>
2025-04-16 12:25:35 +0200

    custom weapon mods

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/CpRedCharacters.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/CpRedCharacters.java
@@ -31,7 +31,7 @@ public class CpRedCharacters {
             referencedColumnName = "id",
             nullable = false
     )
-    private Game game;
+    private Game gameId;
 
     @ManyToOne
     @JoinColumn(
