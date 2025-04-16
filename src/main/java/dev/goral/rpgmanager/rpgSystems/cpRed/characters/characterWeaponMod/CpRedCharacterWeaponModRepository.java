21b1fe9b6fd9f3758dfdae325ddacb0fcf201e4d
MANDRW <mandrzejewski132@gmail.com>
2025-04-16 14:44:03 +0200

    character weapon mod

---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/characters/characterWeaponMod/CpRedCharacterWeaponModRepository.java
@@ -0,0 +1,7 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.characters.characterWeaponMod;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CpRedCharacterWeaponModRepository extends JpaRepository<CpRedCharacterWeaponMod,Long> {
+}
