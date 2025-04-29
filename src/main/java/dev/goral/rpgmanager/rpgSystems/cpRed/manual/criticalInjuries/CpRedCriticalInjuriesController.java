758b461bb2729f406779488f1732d1c2e89a1871
Mateusz Zubrzycki <matiz_z@o2.pl>
2025-04-29 23:56:53 +0200

    Dodałem Service i Controller dla CpRedCriticalInjuries

---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/criticalInjuries/CpRedCriticalInjuriesController.java
@@ -0,0 +1,44 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.manual.criticalInjuries;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "api/v1/authorized")
+@AllArgsConstructor
+public class CpRedCriticalInjuriesController {
+    private CpRedCriticalInjuriesService cpRedCriticalInjuriesService;
+
+    // ============ User methods ============
+    // Pobierz wszystkie obrażenia krytyczne
+    @GetMapping(path = "/rpgSystems/cpRed/criticalInjuries/all")
+    public List<CpRedCriticalInjuriesDTO> getAllCriticalInjuries() {
+        return cpRedCriticalInjuriesService.getAllCriticalInjuries();
+    }
+    // Pobierz obrażenia krytyczne po id
+    @GetMapping(path = "/rpgSystems/cpRed/criticalInjuries/{injuryId}")
+    public CpRedCriticalInjuriesDTO getCriticalInjuryById(@PathVariable("injuryId") Long injuryId) {
+        return cpRedCriticalInjuriesService.getCriticalInjuryById(injuryId);
+    }
+
+    // ============ Admin methods ============
+    // Pobierz wszystkie obrażenia krytyczne dla admina
+    @GetMapping(path = "/admin/rpgSystems/cpRed/criticalInjuries/all")
+    public List<CpRedCriticalInjuries> getAllCriticalInjuriesForAdmin() {
+        return cpRedCriticalInjuriesService.getAllCriticalInjuriesForAdmin();
+    }
+    // Dodać obrażenia krytyczne
+    @PostMapping(path = "/admin/rpgSystems/cpRed/criticalInjuries/add")
+    public Map<String, Object> addCriticalInjury(@RequestBody CpRedCriticalInjuries cpRedCriticalInjuries) {
+        return cpRedCriticalInjuriesService.addCriticalInjury(cpRedCriticalInjuries);
+    }
+    // Modyfikować obrażenia krytyczne
+    @PutMapping(path = "/admin/rpgSystems/cpRed/criticalInjuries/update/{injuryId}")
+    public Map<String, Object> updateCriticalInjury(@PathVariable("injuryId") Long injuryId,
+                                                    @RequestBody CpRedCriticalInjuries cpRedCriticalInjuries) {
+        return cpRedCriticalInjuriesService.updateCriticalInjury(injuryId, cpRedCriticalInjuries);
+    }
+}
---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/criticalInjuries/CpRedCriticalInjuriesService.java
@@ -0,0 +1,39 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.manual.criticalInjuries;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class CpRedCriticalInjuriesService {
+    private final CpRedCriticalInjuriesRepository cpRedCriticalInjuriesRepository;
+
+    // Pobierz wszystkie obrażenia krytyczne
+    public List<CpRedCriticalInjuriesDTO> getAllCriticalInjuries() {
+
+    }
+
+    // Pobierz obrażenia krytyczne po id
+    public CpRedCriticalInjuriesDTO getCriticalInjuryById(Long criticalInjuryId) {
+
+    }
+
+    // Pobierz wszystkie obrażenia krytyczne dla admina
+    public List<CpRedCriticalInjuries> getAllCriticalInjuriesForAdmin() {
+        return cpRedCriticalInjuriesRepository.findAll();
+    }
+
+    // Dodaj obrażenia krytyczne
+    public Map<String, Object> addCriticalInjury(CpRedCriticalInjuries cpRedCriticalInjuries) {
+
+    }
+
+    // Modyfikuj obrażenia krytyczne
+    public Map<String, Object> updateCriticalInjury(Long criticalInjuryId, CpRedCriticalInjuries cpRedCriticalInjuries) {
+
+    }
+
+}
