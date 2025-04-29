ba1e04bb6c06e4063f0e8a85c6d071c4b39da8ff
Mateusz Zubrzycki <matiz_z@o2.pl>
2025-04-29 23:40:26 +0200

    Dodałem Service i Controler do CpRedCyberwares

---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/cyberwares/CpRedCyberwaresService.java
@@ -0,0 +1,38 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.manual.cyberwares;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class CpRedCyberwaresService {
+    private final CpRedCyberwaresRepository cpRedCyberwaresRepository;
+
+    // Pobierz wszystkie cyberware
+    public List<CpRedCyberwaresDTO> getAllCyberwares() {
+
+    }
+
+    // Pobierz cyberware po id
+    public CpRedCyberwaresDTO getCyberwareById(Long cyberwareId) {
+
+    }
+
+    // Pobierz wszystkie cyberware dla admina
+    public List<CpRedCyberwares> getAllCyberwaresForAdmin() {
+        return cpRedCyberwaresRepository.findAll();
+    }
+
+    // Dodać cyberware
+    public Map<String, Object> addCyberware(CpRedCyberwares cpRedCyberwares) {
+
+    }
+
+    // Modyfikować cyberware
+    public Map<String, Object> updateCyberware(Long cyberwareId, CpRedCyberwares cpRedCyberwares) {
+
+    }
+}
