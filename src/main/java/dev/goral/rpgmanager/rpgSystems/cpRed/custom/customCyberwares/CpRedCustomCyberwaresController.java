dba06c161416d196159bbd6d8eef27b355b45ab2
Mateusz Zubrzycki <matiz_z@o2.pl>
2025-04-30 14:02:48 +0200

    Dodałem Service i Controller dla CpRedCustomCyberwares

---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customCyberwares/CpRedCustomCyberwaresController.java
@@ -0,0 +1,45 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customCyberwares;
+
+import dev.goral.rpgmanager.rpgSystems.cpRed.manual.classes.CpRedClassesService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "api/v1/authorized")
+@AllArgsConstructor
+public class CpRedCustomCyberwaresController {
+    private CpRedCustomCyberwaresService cpRedCustomCyberwaresService;
+
+    // ============ User methods ============
+    // Pobierz wszystkie cyberware
+    @GetMapping(path = "/rpgSystems/cpRed/customCyberware/all")
+    public List<CpRedCustomCyberwaresDTO> getAllCyberware() {
+        return cpRedCustomCyberwaresService.getAllCyberware();
+    }
+    // Pobierz cyberware po id
+    @GetMapping(path = "/rpgSystems/cpRed/customCyberware/{cyberwareId}")
+    public CpRedCustomCyberwaresDTO getCyberwareById(@PathVariable("cyberwareId") Long cyberwareId) {
+        return cpRedCustomCyberwaresService.getCyberwareById(cyberwareId);
+    }
+    // Doadaj cyberware
+    @PostMapping(path = "/rpgSystems/cpRed/customCyberware/add")
+    public Map<String, Object> addCyberware(@RequestBody CpRedCustomCyberwares cpRedCustomCyberwares) {
+        return cpRedCustomCyberwaresService.addCyberware(cpRedCustomCyberwares);
+    }
+    // Modyfikować cyberware
+    @PutMapping(path = "/rpgSystems/cpRed/customCyberware/update/{cyberwareId}")
+    public Map<String, Object> updateCyberware(@PathVariable("cyberwareId") Long cyberwareId,
+                                               @RequestBody CpRedCustomCyberwares cpRedCustomCyberwares) {
+        return cpRedCustomCyberwaresService.updateCyberware(cyberwareId, cpRedCustomCyberwares);
+    }
+
+    // ============ Admin methods ============
+    // Pobierz wszystkie cyberware dla admina
+    @GetMapping(path = "/admin/rpgSystems/cpRed/customCyberware/all")
+    public List<CpRedCustomCyberwares> getAllCyberwareForAdmin() {
+        return cpRedCustomCyberwaresService.getAllCyberwareForAdmin();
+    }
+}
---
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customCyberwares/CpRedCustomCyberwaresService.java
@@ -0,0 +1,38 @@
+package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customCyberwares;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class CpRedCustomCyberwaresService {
+    private final CpRedCustomCyberwaresRepository cpRedCustomCyberwaresRepository;
+
+    // Pobierz wszystkie cyberware
+    public List<CpRedCustomCyberwaresDTO> getAllCyberware() {
+
+    }
+
+    // Pobierz cyberware po id
+    public CpRedCustomCyberwaresDTO getCyberwareById(Long cyberwareId) {
+        
+    }
+
+    // Doadaj cyberware
+    public Map<String, Object> addCyberware(CpRedCustomCyberwares cpRedCustomCyberwares) {
+
+    }
+
+    // Modyfikować cyberware
+    public Map<String, Object> updateCyberware(Long cyberwareId, CpRedCustomCyberwares cpRedCustomCyberwares) {
+
+    }
+
+    // Pobierz wszystkie cyberware dla admina
+    public List<CpRedCustomCyberwares> getAllCyberwareForAdmin() {
+        return cpRedCustomCyberwaresRepository.findAll();
+    }
+}
