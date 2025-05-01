7783aae740c6248d91b44bcefe267c0e34816961
Mateusz Zubrzycki <matiz_z@o2.pl>
2025-05-02 00:35:44 +0200

    Pierwsza wersja endpointów

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/stats/CpRedStatsController.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/stats/CpRedStatsController.java
@@ -12,33 +12,33 @@ import java.util.Map;
 public class CpRedStatsController {
     private final CpRedStatsService cpRedStatsService;
 
-//    // ============ User methods ============
-//    // Pobierz wszystkie statystyki
-//    @GetMapping(path = "/rpgSystems/cpRed/stats/all")
-//    public List<CpRedStatsDTO> getAllStats() {
-//        return cpRedStatsService.getAllStats();
-//    }
-//    // Pobierz statystykę po id
-//    @GetMapping(path = "/rpgSystems/cpRed/stats/{statId}")
-//    public CpRedStatsDTO getStatById(@PathVariable("statId") Long statId) {
-//        return cpRedStatsService.getStatById(statId);
-//    }
-//
-//    // ============ Admin methods ============
-//    // Pobierz wszystkie statystyki dla admina
-//    @GetMapping(path = "/admin/rpgSystems/cpRed/stats/all")
-//    public List<CpRedStats> getAllStatsForAdmin() {
-//        return cpRedStatsService.getAllStatsForAdmin();
-//    }
-//    // Dodać statystykę
-//    @GetMapping(path = "/admin/rpgSystems/cpRed/stats/add")
-//    public Map<String, Object> addStat(CpRedStats cpRedStats) {
-//        return cpRedStatsService.addStat(cpRedStats);
-//    }
-//    // Modyfikować statystykę
-//    @GetMapping(path = "/admin/rpgSystems/cpRed/stats/update/{statId}")
-//    public Map<String, Object> updateStat(@PathVariable("statId") Long statId,
-//                                          @RequestBody CpRedStats cpRedStats) {
-//        return cpRedStatsService.updateStat(statId, cpRedStats);
-//    }
+    // ============ User methods ============
+    // Pobierz wszystkie statystyki
+    @GetMapping(path = "/rpgSystems/cpRed/stats/all")
+    public List<CpRedStatsDTO> getAllStats() {
+        return cpRedStatsService.getAllStats();
+    }
+    // Pobierz statystykę po id
+    @GetMapping(path = "/rpgSystems/cpRed/stats/{statId}")
+    public CpRedStatsDTO getStatById(@PathVariable("statId") Long statId) {
+        return cpRedStatsService.getStatById(statId);
+    }
+
+    // ============ Admin methods ============
+    // Pobierz wszystkie statystyki dla admina
+    @GetMapping(path = "/admin/rpgSystems/cpRed/stats/all")
+    public List<CpRedStats> getAllStatsForAdmin() {
+        return cpRedStatsService.getAllStatsForAdmin();
+    }
+    // Dodać statystykę
+    @GetMapping(path = "/admin/rpgSystems/cpRed/stats/add")
+    public Map<String, Object> addStat(CpRedStats cpRedStats) {
+        return cpRedStatsService.addStat(cpRedStats);
+    }
+    // Modyfikować statystykę
+    @GetMapping(path = "/admin/rpgSystems/cpRed/stats/update/{statId}")
+    public Map<String, Object> updateStat(@PathVariable("statId") Long statId,
+                                          @RequestBody CpRedStats cpRedStats) {
+        return cpRedStatsService.updateStat(statId, cpRedStats);
+    }
 }
--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/stats/CpRedStatsRepository.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/stats/CpRedStatsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CpRedStatsRepository extends JpaRepository<CpRedStats, Long> {
 
-
+    boolean existsByName(String name);
 }
