1a8a0ab0e5ff5bc19011513a40d1186ffe90fcee
Mateusz Zubrzycki <matiz_z@o2.pl>
2025-05-04 22:34:03 +0200

    Pierwsza wersja endlopintów w Ammunition
    Wstępnie przetestowane gotowe dla testerów

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/ammunition/CpRedAmmunitionRepository.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/ammunition/CpRedAmmunitionRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CpRedAmmunitionRepository extends JpaRepository<CpRedAmmunition, Long> {
 
+    boolean existsByName(String name);
 }
