b501fbc434d96ed78a0b7eb3f1e27b3d4e350403
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-05-07 21:04:14 +0200

    all services

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/armors/CpRedArmorsService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/armors/CpRedArmorsService.java
@@ -93,9 +93,41 @@ public class CpRedArmorsService {
         cpRedArmorsRepository.save(newArmor);
         return CustomReturnables.getOkResponseMap("Pancerz został dodany");
     }
-//
 //    // Modyfikować pancerz
-//    public Map<String, Object> updateArmor(Long armorId, CpRedArmors cpRedArmors) {
-//
-//    }
+    public Map<String, Object> updateArmor(Long armorId, CpRedArmors armor) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User currentUser = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new ResourceNotFoundException("Zalogowany użytkownik nie został znaleziony."));
+        if (!currentUser.getRole().equals("ROLE_ADMIN")) {
+            throw new IllegalStateException("Nie masz uprawnień do przeglądania tej sekcji.");
+        }
+        if(armor.getType() == null ||
+                armor.getArmorPoints() == 0 ||
+                armor.getPenalty() == 0 ||
+                armor.getPrice() == 0 ||
+                armor.getAvailability() == null ||
+                armor.getDescription() == null) {
+            throw new IllegalStateException("Nie podano wszystkich parametrów");
+        }
+        if(armor.getArmorPoints() < 0 ||
+                armor.getPenalty() < 0 ||
+                armor.getPrice() < 0) {
+            throw new IllegalStateException("Parametry nie mogą być ujemne");
+        }
+        String description = armor.getDescription();
+        if(description.length() > 1000) {
+            throw new IllegalStateException("Opis nie może być dłuższy niż 1000 znaków");
+        }
+        CpRedArmors armorToUpdate = cpRedArmorsRepository.findById(armorId)
+                .orElseThrow(() -> new IllegalStateException("Armor with id " + armorId + " does not exist"));
+        armorToUpdate.setType(armor.getType());
+        armorToUpdate.setArmorPoints(armor.getArmorPoints());
+        armorToUpdate.setPenalty(armor.getPenalty());
+        armorToUpdate.setPrice(armor.getPrice());
+        armorToUpdate.setAvailability(armor.getAvailability());
+        armorToUpdate.setDescription(armor.getDescription());
+        cpRedArmorsRepository.save(armorToUpdate);
+        return CustomReturnables.getOkResponseMap("Pancerz został zaktualizowany");
+    }
 }
