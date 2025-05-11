f172b0a6e17b2c90b26957c000465a31a7618e8f
MANDRW <mandrzejewski132@gmail.com>
2025-05-11 12:49:54 +0200

    service get all cyberwares

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/cyberwares/CpRedCyberwaresService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/cyberwares/CpRedCyberwaresService.java
@@ -3,18 +3,33 @@ package dev.goral.rpgmanager.rpgSystems.cpRed.manual.cyberwares;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @AllArgsConstructor
 public class CpRedCyberwaresService {
     private final CpRedCyberwaresRepository cpRedCyberwaresRepository;
 
-//    // Pobierz wszystkie cyberware
-//    public List<CpRedCyberwaresDTO> getAllCyberwares() {
-//
-//    }
+    // Pobierz wszystkie cyberware
+    public List<CpRedCyberwaresDTO> getAllCyberwares() {
+        List<CpRedCyberwares> cpRedCyberwaresList= cpRedCyberwaresRepository.findAll();
+        return cpRedCyberwaresList.stream().map(
+                cpRedCyberwares-> new CpRedCyberwaresDTO(
+                cpRedCyberwares.getName(),
+                cpRedCyberwares.getMountPlace().toString(),
+                cpRedCyberwares.getRequirements(),
+                cpRedCyberwares.getHumanityLoss(),
+                cpRedCyberwares.getSize(),
+                cpRedCyberwares.getInstallationPlace().toString(),
+                cpRedCyberwares.getPrice(),
+                cpRedCyberwares.getAvailability().toString(),
+                cpRedCyberwares.getDescription()
+        )).toList();
+    }
 //
 //    // Pobierz cyberware po id
 //    public CpRedCyberwaresDTO getCyberwareById(Long cyberwareId) {
