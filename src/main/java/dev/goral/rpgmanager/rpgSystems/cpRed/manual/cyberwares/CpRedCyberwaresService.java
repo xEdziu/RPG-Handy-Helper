711d9a507c613cb94b639392fde726d8cf428118
MANDRW <mandrzejewski132@gmail.com>
2025-05-11 14:45:52 +0200

    service add cyberwares

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/cyberwares/CpRedCyberwaresService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/cyberwares/CpRedCyberwaresService.java
@@ -1,7 +1,13 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.manual.cyberwares;
 
+import dev.goral.rpgmanager.security.CustomReturnables;
 import dev.goral.rpgmanager.security.exceptions.ResourceNotFoundException;
+import dev.goral.rpgmanager.user.User;
+import dev.goral.rpgmanager.user.UserRepository;
+import dev.goral.rpgmanager.user.UserRole;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,6 +20,7 @@ import static java.util.stream.Collectors.toList;
 @AllArgsConstructor
 public class CpRedCyberwaresService {
     private final CpRedCyberwaresRepository cpRedCyberwaresRepository;
+    private final UserRepository userRepository;
 
     // Pobierz wszystkie cyberware
     public List<CpRedCyberwaresDTO> getAllCyberwares() {
@@ -50,11 +57,62 @@ public class CpRedCyberwaresService {
     public List<CpRedCyberwares> getAllCyberwaresForAdmin() {
         return cpRedCyberwaresRepository.findAll();
     }
-//
-//    // Dodać cyberware
-//    public Map<String, Object> addCyberware(CpRedCyberwares cpRedCyberwares) {
-//
-//    }
+
+    // Dodać cyberware
+    public Map<String, Object> addCyberware(CpRedCyberwares cpRedCyberwares) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User currentUser = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new ResourceNotFoundException("Zalogowany użytkownik nie został znaleziony."));
+
+        if (!currentUser.getRole().equals(UserRole.ROLE_ADMIN)) {
+            throw new IllegalStateException("Nie masz uprawnień do dodawania wszczepów.");
+        }
+        if(cpRedCyberwares.getName()==null ||
+        cpRedCyberwares.getMountPlace().toString()==null ||
+                cpRedCyberwares.getRequirements()==null ||
+                cpRedCyberwares.getHumanityLoss()==null ||
+                cpRedCyberwares.getSize()==-1 ||
+                cpRedCyberwares.getInstallationPlace().toString()==null ||
+                cpRedCyberwares.getPrice()==-1 ||
+                cpRedCyberwares.getAvailability().toString()==null ||
+                cpRedCyberwares.getDescription()==null) {
+            throw new IllegalStateException("Nie wszystkie pola zostały wypełnione.");
+
+        }
+        if(cpRedCyberwaresRepository.existsByName(cpRedCyberwares.getName())) {
+            throw new IllegalStateException("Wszczep o tej nazwie już istnieje.");
+        }
+        if (cpRedCyberwares.getName().isEmpty() || cpRedCyberwares.getName().trim().isEmpty()) {
+            throw new IllegalStateException("Nazwa broni nie może być pusta.");
+        }
+        if (cpRedCyberwares.getName().length() > 255) {
+            throw new IllegalStateException("Nazwa wszczepu nie może być dłuższa niż 255 znaków.");
+        }
+        if (cpRedCyberwares.getPrice() < 0) {
+            throw new IllegalStateException("Cena nie może być ujemna.");
+        }
+        if (cpRedCyberwares.getDescription().isEmpty() || cpRedCyberwares.getDescription().trim().isEmpty()) {
+            throw new IllegalStateException("Opis wszczepu nie może być pusty.");
+        }
+        if (cpRedCyberwares.getDescription().length() > 500) {
+            throw new IllegalStateException("Opis wszczepu nie może być dłuższy niż 500 znaków.");
+        }
+        CpRedCyberwares newCyberware = new CpRedCyberwares(
+                null,
+                cpRedCyberwares.getName(),
+                cpRedCyberwares.getMountPlace(),
+                cpRedCyberwares.getRequirements(),
+                cpRedCyberwares.getHumanityLoss(),
+                cpRedCyberwares.getSize(),
+                cpRedCyberwares.getInstallationPlace(),
+                cpRedCyberwares.getPrice(),
+                cpRedCyberwares.getAvailability(),
+                cpRedCyberwares.getDescription()
+        );
+        cpRedCyberwaresRepository.save(newCyberware);
+        return CustomReturnables.getOkResponseMap("Wszczep został dodany.");
+    }
 //
 //    // Modyfikować cyberware
 //    public Map<String, Object> updateCyberware(Long cyberwareId, CpRedCyberwares cpRedCyberwares) {
