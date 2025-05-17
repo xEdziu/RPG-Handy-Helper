3b0a14510245a3676b0f65bf46f91bbe32d3029e
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-05-18 00:59:30 +0200

    request class and two new metods

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customArmors/CpRedCustomArmorsRepository.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customArmors/CpRedCustomArmorsRepository.java
@@ -3,6 +3,10 @@ package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customArmors;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CpRedCustomArmorsRepository extends JpaRepository<CpRedCustomArmors, Long> {
+    List<CpRedCustomArmors> findAllByGameId(Long gameId);
+
 }
--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customArmors/CpRedCustomArmorsService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customArmors/CpRedCustomArmorsService.java
@@ -1,8 +1,17 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customArmors;
 
+import dev.goral.rpgmanager.game.Game;
+import dev.goral.rpgmanager.game.GameRepository;
+import dev.goral.rpgmanager.game.gameUsers.GameUsers;
+import dev.goral.rpgmanager.game.gameUsers.GameUsersRepository;
 import dev.goral.rpgmanager.security.CustomReturnables;
+import dev.goral.rpgmanager.security.exceptions.ResourceNotFoundException;
+import dev.goral.rpgmanager.user.User;
+import dev.goral.rpgmanager.user.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
 import java.util.Map;
@@ -11,6 +20,9 @@ import java.util.Map;
 @AllArgsConstructor
 public class CpRedCustomArmorsService {
     private final CpRedCustomArmorsRepository cpRedCustomArmorsRepository;
+    private final GameRepository gameRepository;
+    private final UserRepository userRepository;
+    private final GameUsersRepository gameUsersRepository;
 
     // Pobierz wszystkie customowe zbroje
     public Map<String, Object> getAllCustomArmors() {
@@ -41,14 +53,42 @@ public class CpRedCustomArmorsService {
                         CpRedCustomArmors.getPrice(),
                         CpRedCustomArmors.getAvailability().toString(),
                         CpRedCustomArmors.getDescription()
-                )).orElse(null);
+                )).orElseThrow(() -> new ResourceNotFoundException("Customowy pancerz o id " + armorId + " nie istnieje."));
         Map<String,Object> response = CustomReturnables.getOkResponseMap("Customowa zbroja została pobrana.");
         response.put("customArmor", customArmor);
         return response;
     }
+    public Map<String, Object> getCustomArmorByGame(Long gameId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User currentUser = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new ResourceNotFoundException("Zalogowany użytkownik nie został znaleziony."));
+
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gra o id " + gameId + " nie istnieje."));
+
+        GameUsers gameUsers = gameUsersRepository.findGameUsersByUserIdAndGameId(currentUser.getId(), gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie jesteś graczem wybranej gry."));
+        List<CpRedCustomArmorsDTO> gameCustomArmorsList = cpRedCustomArmorsRepository.findAllByGameId(gameId).stream()
+                .map(CpRedCustomArmors-> new CpRedCustomArmorsDTO(
+                        CpRedCustomArmors.getGameId().getId(),
+                        CpRedCustomArmors.getType().toString(),
+                        CpRedCustomArmors.getArmorPoints(),
+                        CpRedCustomArmors.getPenalty(),
+                        CpRedCustomArmors.getPrice(),
+                        CpRedCustomArmors.getAvailability().toString(),
+                        CpRedCustomArmors.getDescription()
+                )).toList();
+
+        Map<String,Object> response = CustomReturnables.getOkResponseMap("Customowe zbroje do gry zostały pobrane.");
+        response.put("customArmor", gameCustomArmorsList);
+        return response;
+    }
+
+
+    // Dodaj customową zbroję
+//    public Map<String, Object> addCustomArmor(CpRedCustomArmorsRequest cpRedCustomArmors) {
 //
-//    // Dodaj customową zbroję
-//    public Map<String, Object> addCustomArmor(CpRedCustomArmors cpRedCustomArmors) {
 //
 //    }
 //
@@ -57,8 +97,12 @@ public class CpRedCustomArmorsService {
 //
 //    }
 //
-//    // Pobierz wszystkie customowe zbroje dla admina
-//    public Map<String, Object> getAllCustomArmorsForAdmin() {
-//
-//    }
+    // Pobierz wszystkie customowe zbroje dla admina
+    public Map<String, Object> getAllCustomArmorsForAdmin() {
+        List<CpRedCustomArmors> allCustomArmorsList = cpRedCustomArmorsRepository.findAll();
+
+        Map<String, Object> response = CustomReturnables.getOkResponseMap("Customowe pancerze zostały pobrane dla administratora.");
+        response.put("customArmors", allCustomArmorsList);
+        return response;
+    }
 }
