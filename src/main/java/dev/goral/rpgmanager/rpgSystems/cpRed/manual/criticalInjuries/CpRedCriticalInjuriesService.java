144ff365d2899adca4feed001ef253cb34a21506
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-05-19 11:28:07 +0200

    feat(CpRedCriticalInjuriesService): implement method to retrieve all critical injuries

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/criticalInjuries/CpRedCriticalInjuriesService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/criticalInjuries/CpRedCriticalInjuriesService.java
@@ -1,5 +1,6 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.manual.criticalInjuries;
 
+import dev.goral.rpgmanager.security.CustomReturnables;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,22 @@ import java.util.Map;
 public class CpRedCriticalInjuriesService {
     private final CpRedCriticalInjuriesRepository cpRedCriticalInjuriesRepository;
 
-//    // Pobierz wszystkie obrażenia krytyczne
-//    public Map<String, Object> getAllCriticalInjuries() {
-//
-//    }
+    // Pobierz wszystkie obrażenia krytyczne
+    public Map<String, Object> getAllCriticalInjuries() {
+        List<CpRedCriticalInjuriesDTO> cpRedCriticalInjuriesList = cpRedCriticalInjuriesRepository.findAll().stream().
+                map(cpRedCriticalInjuries -> new CpRedCriticalInjuriesDTO(
+                        cpRedCriticalInjuries.getRollValue(),
+                        cpRedCriticalInjuries.getInjuryPlace().toString(),
+                        cpRedCriticalInjuries.getName(),
+                        cpRedCriticalInjuries.getEffects(),
+                        cpRedCriticalInjuries.getPatching(),
+                        cpRedCriticalInjuries.getTreating()
+                )).toList();
+        Map<String, Object> response = CustomReturnables.getOkResponseMap("Pobrano wszystkie rodzaje obrażeń krytycznych.");
+        response.put("criticalInjuries", cpRedCriticalInjuriesList);
+        return response;
+
+    }
 //
 //    // Pobierz obrażenia krytyczne po id
 //    public Map<String, Object> getCriticalInjuryById(Long criticalInjuryId) {
