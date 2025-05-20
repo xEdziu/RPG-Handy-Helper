e5319047ce308d9587add3c30e9636ef479f1f71
MANDRW <mandrzejewski132@gmail.com>
2025-05-20 23:22:44 +0200

    feat: implement getCyberwareById and getCyberwareByGame methods to retrieve custom cyberwares

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customCyberwares/CpRedCustomCyberwaresService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customCyberwares/CpRedCustomCyberwaresService.java
@@ -1,14 +1,19 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customCyberwares;
 
+import dev.goral.rpgmanager.game.Game;
 import dev.goral.rpgmanager.game.GameRepository;
+import dev.goral.rpgmanager.game.gameUsers.GameUsers;
 import dev.goral.rpgmanager.game.gameUsers.GameUsersRepository;
 import dev.goral.rpgmanager.rpgSystems.cpRed.custom.customArmors.CpRedCustomArmors;
 import dev.goral.rpgmanager.rpgSystems.cpRed.custom.customArmors.CpRedCustomArmorsService;
 import dev.goral.rpgmanager.rpgSystems.cpRed.custom.customCriticalInjuries.CpRedCustomCriticalInjuries;
 import dev.goral.rpgmanager.security.CustomReturnables;
 import dev.goral.rpgmanager.security.exceptions.ResourceNotFoundException;
+import dev.goral.rpgmanager.user.User;
 import dev.goral.rpgmanager.user.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -62,6 +67,35 @@ public class CpRedCustomCyberwaresService {
         return response;
 
     }
+    public Map<String, Object> getCyberwareByGame(Long gameId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User currentUser = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new ResourceNotFoundException("Zalogowany użytkownik nie został znaleziony."));
+
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gra o id " + gameId + " nie istnieje."));
+
+        GameUsers gameUsers = gameUsersRepository.findGameUsersByUserIdAndGameId(currentUser.getId(), gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie jesteś graczem wybranej gry."));
+        List<CpRedCustomCyberwaresDTO> allCustomCyberwares=cpRedCustomCyberwaresRepository.findAllByGameId(game).stream()
+                .map(CpRedCustomCyberwares -> new CpRedCustomCyberwaresDTO(
+                        CpRedCustomCyberwares.getGameId().getId(),
+                        CpRedCustomCyberwares.getName(),
+                        CpRedCustomCyberwares.getMountPlace().toString(),
+                        CpRedCustomCyberwares.getRequirements(),
+                        CpRedCustomCyberwares.getHumanityLoss(),
+                        CpRedCustomCyberwares.getSize(),
+                        CpRedCustomCyberwares.getInstallationPlace().toString(),
+                        CpRedCustomCyberwares.getPrice(),
+                        CpRedCustomCyberwares.getAvailability().toString(),
+                        CpRedCustomCyberwares.getDescription()
+                )).toList();
+        Map<String,Object> response= CustomReturnables.getOkResponseMap("Customowe wszczepy do gry zostały pobrane.");
+        response.put("customCyberwares",allCustomCyberwares);
+        return response;
+    }
+
 //
 //    // Doadaj cyberware
 //    public Map<String, Object> addCyberware(CpRedCustomCyberwares cpRedCustomCyberwares) {
