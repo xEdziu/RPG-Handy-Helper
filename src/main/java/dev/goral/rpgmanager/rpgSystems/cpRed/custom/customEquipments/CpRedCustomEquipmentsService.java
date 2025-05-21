7ac57624994ec1facf609cc31f6bc7df9e76d79b
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-05-21 20:06:03 +0200

    get all custom eq metod

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customEquipments/CpRedCustomEquipmentsService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customEquipments/CpRedCustomEquipmentsService.java
@@ -1,38 +1,47 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customEquipments;
 
+import dev.goral.rpgmanager.security.CustomReturnables;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @AllArgsConstructor
 public class CpRedCustomEquipmentsService {
     private final CpRedCustomEquipmentsRepository cpRedCustomEquipmentsRepository;
 
-//    // Pobierz wszystkie przedmioty
-//    public Map<String, Object> getAllEquipments(){
-//
-//    }
-//
-//    // Pobierz przedmiot po id
-//    public Map<String, Object> getEquipmentById(Long equipmentId){
-//
-//    }
-//
-//    // Dodaj przedmiot
-//    public Map<String, Object> addEquipment(CpRedCustomEquipments cpRedCustomEquipments){
-//
-//    }
-//
-//    // Modyfikować przedmiot
-//    public Map<String, Object> updateEquipment(Long equipmentId, CpRedCustomEquipments cpRedCustomEquipments){
-//
-//    }
-//
-//    // Pobierz wszystkie przedmioty dla admina
-//    public Map<String, Object> getAllEquipmentsForAdmin(){
-//
-//    }
+    public Map<String, Object> getAllEquipments(){
+        List<CpRedCustomEquipmentsDTO> allCustomEquipments = cpRedCustomEquipmentsRepository.findAll().stream()
+                .map(CpRedCustomEquipments -> new CpRedCustomEquipmentsDTO(
+                        CpRedCustomEquipments.getGameId().getId(),
+                        CpRedCustomEquipments.getName(),
+                        CpRedCustomEquipments.getPrice(),
+                        CpRedCustomEquipments.getAvailability().toString(),
+                        CpRedCustomEquipments.getDescription()
+                )).toList();
+        Map<String, Object> response = CustomReturnables.getOkResponseMap("Customowe wyposażenie  zostały pobrane.");
+        response.put("customEquipments", allCustomEquipments);
+        return response;
+    }
+
+    public Map<String, Object> getEquipmentById(Long equipmentId){
+
+    }
+
+    public Map<String, Object> addEquipment(CpRedCustomEquipmentsRequest cpRedCustomEquipments){
+
+    }
+
+    public Map<String, Object> updateEquipment(Long equipmentId, CpRedCustomEquipmentsRequest cpRedCustomEquipments){
+
+    }
+
+    public Map<String, Object> getAllEquipmentsForAdmin(){
+
+    }
 }
