c333260668e90b45bb6d01b6ead83584da4c7fa0
MANDRW <mandrzejewski132@gmail.com>
2025-05-26 14:35:10 +0200

    feat(CpRedWeaponModsService): implement retrieval of all weapon modifications with DTO mapping

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/weaponMods/CpRedWeaponModsService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/manual/weaponMods/CpRedWeaponModsService.java
@@ -1,20 +1,40 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.manual.weaponMods;
 
+import dev.goral.rpgmanager.rpgSystems.cpRed.manual.armors.CpRedArmors;
+import dev.goral.rpgmanager.rpgSystems.cpRed.manual.armors.CpRedArmorsDTO;
+import dev.goral.rpgmanager.security.CustomReturnables;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toList;
+
 @Service
 @AllArgsConstructor
 public class CpRedWeaponModsService {
     private final CpRedWeaponModsRepository cpRedWeaponModsRepository;
 
-//    // Pobierz wszystkie modyfikacje broni
-//    public Map<String, Object> getAllWeaponMods() {
-//
-//    }
+    // Pobierz wszystkie modyfikacje broni
+    public Map<String, Object> getAllWeaponMods() {
+        List<CpRedWeaponMods> mods= cpRedWeaponModsRepository.findAll();
+        List<CpRedWeaponModsDTO> modsDTO = mods.stream().map( mod->
+                new CpRedWeaponModsDTO(
+                        mod.getName(),
+                        mod.getPrice(),
+                        mod.getSize(),
+                        mod.getAvailability().toString(),
+                        mod.getDescription()
+
+                )).toList();
+        if(modsDTO.isEmpty()){
+            return CustomReturnables.getOkResponseMap("Brak modyfikacji broni");
+        }
+        Map<String,Object> response = CustomReturnables.getOkResponseMap("Pobrano modyfikacje broni");
+        response.put("weaponMods", modsDTO);
+        return response;
+    }
 //
 //    // Pobierz modyfikacje broni po id
 //    public Map<String, Object> getWeaponModById(Long weaponModId) {
