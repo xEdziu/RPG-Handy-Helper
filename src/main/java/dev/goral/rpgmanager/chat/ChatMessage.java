46042a8522f7842bf0493295019914496d3d748f
Adrian Goral <adrian.goral@gmail.com>
2025-05-26 19:55:43 +0200

    Implement private messaging feature in chat: add handling for private messages and update message display in room.html

--- src/main/java/dev/goral/rpgmanager/chat/ChatMessage.java
+++ src/main/java/dev/goral/rpgmanager/chat/ChatMessage.java
@@ -1,9 +1,14 @@
 package dev.goral.rpgmanager.chat;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatMessage {
     private String from;
     private String content;
+    private Boolean privateMessage;
 }
