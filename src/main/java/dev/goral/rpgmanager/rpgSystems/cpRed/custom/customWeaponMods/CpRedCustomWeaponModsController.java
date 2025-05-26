e61593a3156e5c00ca76fb39dc0defc91821973d
MANDRW <mandrzejewski132@gmail.com>
2025-05-26 16:41:35 +0200

    feat(CpRedCustomWeaponModsController): restore user and admin methods for weapon modifications

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customWeaponMods/CpRedCustomWeaponModsController.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customWeaponMods/CpRedCustomWeaponModsController.java
@@ -12,33 +12,29 @@ import java.util.Map;
 public class CpRedCustomWeaponModsController {
     private final CpRedCustomWeaponModsService cpRedCustomWeaponModsService;
 
-//    // ============ User methods ============
-//    // Pobierz wszystkie modyfikacje broni
-//    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/all")
-//    public LMap<String, Object> getAllWeaponMods() { // List<CpRedCustomWeaponModsDTO>
-//        return cpRedCustomWeaponModsService.getAllWeaponMods();
-//    }
-//    // Pobierz modyfikację broni po id
-//    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/{weaponModId}")
-//    public Map<String, Object> getWeaponModById(@PathVariable("weaponModId") Long weaponModId) { // CpRedCustomWeaponModsDTO
-//        return cpRedCustomWeaponModsService.getWeaponModById(weaponModId);
-//    }
-//    // Dodaj modyfikację broni
-//    @PostMapping(path = "/rpgSystems/cpRed/custom/weaponMods/add")
-//    public Map<String, Object> addWeaponMod(@RequestBody CpRedCustomWeaponMods cpRedCustomWeaponMods) {
-//        return cpRedCustomWeaponModsService.addWeaponMod(cpRedCustomWeaponMods);
-//    }
-//    // Modyfikować modyfikację broni
-//    @PutMapping(path = "/rpgSystems/cpRed/custom/weaponMods/update/{weaponModId}")
-//    public Map<String, Object> updateWeaponMod(@PathVariable("weaponModId") Long weaponModId,
-//                                               @RequestBody CpRedCustomWeaponMods cpRedCustomWeaponMods) {
-//         return cpRedCustomWeaponModsService.updateWeaponMod(weaponModId, cpRedCustomWeaponMods);
-//     }
-//
-//    // ============ Admin methods ============
-//    // Pobierz wszystkie modyfikacje broni dla admina
-//    @GetMapping(path = "/admin/rpgSystems/cpRed/custom/weaponMods/all")
-//    public Map<String, Object> getAllWeaponModsForAdmin() { // List<CpRedCustomWeaponMods>
-//        return cpRedCustomWeaponModsService.getAllWeaponModsForAdmin();
-//    }
+    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/all")
+    public Map<String, Object> getAllWeaponMods() {
+        return cpRedCustomWeaponModsService.getAllWeaponMods();
+    }
+
+    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/{weaponModId}")
+    public Map<String, Object> getWeaponModById(@PathVariable("weaponModId") Long weaponModId) {
+        return cpRedCustomWeaponModsService.getWeaponModById(weaponModId);
+    }
+
+    @PostMapping(path = "/rpgSystems/cpRed/custom/weaponMods/add")
+    public Map<String, Object> addWeaponMod(@RequestBody CpRedCustomWeaponModsRequest cpRedCustomWeaponMods) {
+        return cpRedCustomWeaponModsService.addWeaponMod(cpRedCustomWeaponMods);
+    }
+
+    @PutMapping(path = "/rpgSystems/cpRed/custom/weaponMods/update/{weaponModId}")
+    public Map<String, Object> updateWeaponMod(@PathVariable("weaponModId") Long weaponModId,
+                                               @RequestBody CpRedCustomWeaponModsRequest cpRedCustomWeaponMods) {
+         return cpRedCustomWeaponModsService.updateWeaponMod(weaponModId, cpRedCustomWeaponMods);
+     }
+
+    @GetMapping(path = "/admin/rpgSystems/cpRed/custom/weaponMods/all")
+    public Map<String, Object> getAllWeaponModsForAdmin() {
+        return cpRedCustomWeaponModsService.getAllWeaponModsForAdmin();
+    }
 }
