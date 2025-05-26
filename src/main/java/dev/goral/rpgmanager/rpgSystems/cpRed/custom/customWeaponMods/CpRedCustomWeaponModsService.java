d8ce5271479e52ec1f1cd3da979c8b10fa429aa4
MANDRW <mandrzejewski132@gmail.com>
2025-05-26 16:08:06 +0200

    feat(CpRedCustomWeaponModsService): add method to retrieve weapon modifications by game ID with user validation

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customWeaponMods/CpRedCustomWeaponModsService.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customWeaponMods/CpRedCustomWeaponModsService.java
@@ -1,8 +1,16 @@
 package dev.goral.rpgmanager.rpgSystems.cpRed.custom.customWeaponMods;
 
+import dev.goral.rpgmanager.game.Game;
+import dev.goral.rpgmanager.game.GameRepository;
+import dev.goral.rpgmanager.game.gameUsers.GameUsers;
+import dev.goral.rpgmanager.game.gameUsers.GameUsersRepository;
 import dev.goral.rpgmanager.security.CustomReturnables;
 import dev.goral.rpgmanager.security.exceptions.ResourceNotFoundException;
+import dev.goral.rpgmanager.user.User;
+import dev.goral.rpgmanager.user.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -13,6 +21,10 @@ import java.util.Map;
 @AllArgsConstructor
 public class CpRedCustomWeaponModsService {
     private final CpRedCustomWeaponModsRepository cpRedCustomWeaponModsRepository;
+    private final GameUsersRepository gameUsersRepository;
+    private final UserRepository userRepository;
+    private final GameRepository gameRepository;
+
 
 
     // Pobierz wszystkie modyfikacje broni
@@ -46,6 +58,34 @@ public class CpRedCustomWeaponModsService {
         response.put("customWeaponMod", cpRedCustomWeaponMods);
         return response;
     }
+
+    public Map<String, Object> getWeaponModsByGame(Long gameId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User currentUser = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new ResourceNotFoundException("Zalogowany użytkownik nie został znaleziony."));
+
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gra o id " + gameId + " nie istnieje."));
+
+        GameUsers gameUsers = gameUsersRepository.findGameUsersByUserIdAndGameId(currentUser.getId(), gameId)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie jesteś graczem wybranej gry."));
+        List<CpRedCustomWeaponModsDTO> allCustomMods=cpRedCustomWeaponModsRepository.findAllByGameId(game).stream()
+                .map(CpRedCustomWeaponMods -> new CpRedCustomWeaponModsDTO(
+                        CpRedCustomWeaponMods.getGameId().getId(),
+                        CpRedCustomWeaponMods.getName(),
+                        CpRedCustomWeaponMods.getPrice(),
+                        CpRedCustomWeaponMods.getSize(),
+                        CpRedCustomWeaponMods.getAvailability().toString(),
+                        CpRedCustomWeaponMods.getDescription()
+                )).toList();
+        Map<String,Object> response= CustomReturnables.getOkResponseMap("Customowe modyfikacje do broni, do gry zostały pobrane.");
+        response.put("customMods",allCustomMods);
+        return response;
+    }
+
+
+
 //
 //    // Dodaj modyfikację broni
 //    public Map<String, Object> addWeaponMod(CpRedCustomWeaponMods cpRedCustomWeaponMods) {
