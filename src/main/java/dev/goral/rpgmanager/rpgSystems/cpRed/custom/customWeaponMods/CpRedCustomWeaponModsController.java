0a6f8b0fd65c275318db1c0c1bf7104933691ff8
MANDRW <mandrzejewski132@gmail.com>
2025-05-26 16:52:16 +0200

    feat(CpRedCustomWeaponModsController): update endpoint paths for weapon modifications

--- src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customWeaponMods/CpRedCustomWeaponModsController.java
+++ src/main/java/dev/goral/rpgmanager/rpgSystems/cpRed/custom/customWeaponMods/CpRedCustomWeaponModsController.java
@@ -12,33 +12,33 @@ import java.util.Map;
 public class CpRedCustomWeaponModsController {
     private final CpRedCustomWeaponModsService cpRedCustomWeaponModsService;
 
-    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/all")
+    @GetMapping(path = "/rpgSystems/cpRed/customWeaponMods/all")
     public Map<String, Object> getAllWeaponMods() {
         return cpRedCustomWeaponModsService.getAllWeaponMods();
     }
 
-    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/{weaponModId}")
+    @GetMapping(path = "/rpgSystems/cpRed/customWeaponMods/{weaponModId}")
     public Map<String, Object> getWeaponModById(@PathVariable("weaponModId") Long weaponModId) {
         return cpRedCustomWeaponModsService.getWeaponModById(weaponModId);
     }
 
-    @GetMapping(path = "/rpgSystems/cpRed/custom/weaponMods/game/{gameId}")
+    @GetMapping(path = "/rpgSystems/cpRed/customWeaponMods/game/{gameId}")
     public Map<String, Object> getWeaponModsByGame(@PathVariable("gameId") Long gameId) {
         return cpRedCustomWeaponModsService.getWeaponModsByGame(gameId);
     }
 
-    @PostMapping(path = "/rpgSystems/cpRed/custom/weaponMods/add")
+    @PostMapping(path = "/rpgSystems/cpRed/customWeaponMods/add")
     public Map<String, Object> addWeaponMod(@RequestBody CpRedCustomWeaponModsRequest cpRedCustomWeaponMods) {
         return cpRedCustomWeaponModsService.addWeaponMod(cpRedCustomWeaponMods);
     }
 
-    @PutMapping(path = "/rpgSystems/cpRed/custom/weaponMods/update/{weaponModId}")
+    @PutMapping(path = "/rpgSystems/cpRed/customWeaponMods/update/{weaponModId}")
     public Map<String, Object> updateWeaponMod(@PathVariable("weaponModId") Long weaponModId,
                                                @RequestBody CpRedCustomWeaponModsRequest cpRedCustomWeaponMods) {
          return cpRedCustomWeaponModsService.updateWeaponMod(weaponModId, cpRedCustomWeaponMods);
      }
 
-    @GetMapping(path = "/admin/rpgSystems/cpRed/custom/weaponMods/all")
+    @GetMapping(path = "/admin/rpgSystems/cpRed/customWeaponMods/all")
     public Map<String, Object> getAllWeaponModsForAdmin() {
         return cpRedCustomWeaponModsService.getAllWeaponModsForAdmin();
     }
