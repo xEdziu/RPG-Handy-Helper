5ff0b7c6c0a3b2343be9f29c2492143968fc0228
Adrian Goral <adrian.goral@gmail.com>
2025-06-03 00:54:06 +0200

    Add ResourceNotFoundException import to UserService for improved error handling

--- src/main/java/dev/goral/rpghandyhelper/user/UserService.java
+++ src/main/java/dev/goral/rpghandyhelper/user/UserService.java
@@ -1,6 +1,7 @@
 package dev.goral.rpghandyhelper.user;
 
 import dev.goral.rpghandyhelper.security.CustomReturnables;
+import dev.goral.rpghandyhelper.security.exceptions.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
