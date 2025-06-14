3fbc74542a0b07d1222064b940ed9ed6412e3f92
Mateusz Zubrzycki <matiz_z@o2.pl>
2025-06-14 14:50:54 +0200

    Pierwsza działająca wersja

---
+++ src/main/java/dev/goral/rpghandyhelper/rpgSystems/cpRed/characters/characterSkills/UpdateCharacterSkillsRequest.java
@@ -0,0 +1,14 @@
+package dev.goral.rpghandyhelper.rpgSystems.cpRed.characters.characterSkills;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateCharacterSkillsRequest {
+    private Integer skillLevel;
+}
