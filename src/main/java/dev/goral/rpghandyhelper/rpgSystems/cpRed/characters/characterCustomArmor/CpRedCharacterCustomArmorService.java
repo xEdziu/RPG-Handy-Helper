c7ee795c428d4c4524fb34107559216022211315
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-06-14 21:51:37 +0200

    Add delete functionality for character custom armor with access control checks

--- src/main/java/dev/goral/rpghandyhelper/rpgSystems/cpRed/characters/characterCustomArmor/CpRedCharacterCustomArmorService.java
+++ src/main/java/dev/goral/rpghandyhelper/rpgSystems/cpRed/characters/characterCustomArmor/CpRedCharacterCustomArmorService.java
@@ -9,6 +9,7 @@ import dev.goral.rpghandyhelper.game.gameUsers.GameUsersRole;
 import dev.goral.rpghandyhelper.rpgSystems.cpRed.characters.CpRedCharacters;
 import dev.goral.rpghandyhelper.rpgSystems.cpRed.characters.CpRedCharactersRepository;
 import dev.goral.rpghandyhelper.rpgSystems.cpRed.characters.CpRedCharactersType;
+import dev.goral.rpghandyhelper.rpgSystems.cpRed.characters.characterCustomWeapon.CpRedCharacterCustomWeapon;
 import dev.goral.rpghandyhelper.rpgSystems.cpRed.custom.customArmors.CpRedCustomArmors;
 import dev.goral.rpghandyhelper.rpgSystems.cpRed.custom.customArmors.CpRedCustomArmorsRepository;
 import dev.goral.rpghandyhelper.rpgSystems.cpRed.custom.customWeapons.CpRedCustomWeapons;
@@ -173,4 +174,42 @@ public Map<String,Object> updateCharacterCustomArmor(Long characterCustomArmorId
     return CustomReturnables.getOkResponseMap("Customowy pancerz postaci został zmodyfikowany.");
     }
 
+    public Map<String, Object> deleteCharacterCustomArmor(Long characterCustomArmorId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        User currentUser = userRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new ResourceNotFoundException("Zalogowany użytkownik nie został znaleziony."));
+
+        CpRedCharacterCustomArmor characterCustomArmor = cpRedCharacterCustomArmorRepository.findById(characterCustomArmorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Pancerz postaci o podanym ID nie została znaleziona."));
+
+        CpRedCharacters character = cpRedCharactersRepository.findById(characterCustomArmor.getCharacterId().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Postać o podanym ID nie została znaleziona."));
+
+        CpRedCustomArmors armor = cpRedCustomArmorsRepository.findById(characterCustomArmor.getArmorId().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Pancerz o podanym ID nie został znaleziony."));
+
+        Game game = gameRepository.findById(character.getGame().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Gra powiązana z postacią nie została znaleziona."));
+
+        GameUsers gameUsers = gameUsersRepository.findGameUsersByUserIdAndGameId(currentUser.getId(), game.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Nie należysz do gry powiązanej z podaną postacią."));
+
+        if (gameUsers.getRole() != GameUsersRole.GAMEMASTER){
+            if (character.getType() != CpRedCharactersType.NPC) {
+                if (!Objects.equals(currentUser.getId(), character.getUser().getId())) {
+                    throw new ResourceNotFoundException("Zalogowany użytkownik nie jest GM-em w tej grze ani nie jest właścicielem postaci.");
+                }
+            } else {
+                throw new ResourceNotFoundException("Tylko GM może usuwać customowe bronie postaci NPC.");
+            }
+        }
+
+        if (game.getStatus() != GameStatus.ACTIVE) {
+            throw new IllegalStateException("Gra do której należy postać nie jest aktywna.");
+        }
+
+        cpRedCharacterCustomArmorRepository.deleteById(characterCustomArmorId);
+        return CustomReturnables.getOkResponseMap("Customowy pancerz postaci został pomyślnie usunięty.");
+    }
 }
