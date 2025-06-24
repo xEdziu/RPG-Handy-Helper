cff8ae08abddb388afaf845c369a5213b895ac0a
Adrian Goral <adrian.goral@gmail.com>
2025-06-24 13:20:17 +0200

    Add characters page and controller method for game character retrieval

--- src/main/java/dev/goral/rpghandyhelper/content/ContentController.java
+++ src/main/java/dev/goral/rpghandyhelper/content/ContentController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ContentController {
@@ -81,5 +82,11 @@ public class ContentController {
     public String getNotes() {
         System.out.println("ContentController: getNotes()");
         return "home/notes";
+    }
+
+    @GetMapping("/home/games/{gameId}/characters")
+    public String getCharacters(@PathVariable String gameId) {
+        System.out.println("ContentController: getCharacters()");
+        return "home/characters";
     }
 }
